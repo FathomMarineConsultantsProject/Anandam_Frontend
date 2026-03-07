63d1629c9653f34320d6f49fe91721c72d57b5a5
avani-fmc <software2@fathhomai.onmicrosoft.com>
2026-03-07 20:50:32 +0530

    feat: integrate dashboard layout with hero, features, and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import HomePage from './pages/HomePage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
