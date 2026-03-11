7f55585c57dab58a55d541026e6066f85d41014b
avani-fmc <software2@fathhomai.onmicrosoft.com>
2026-03-11 17:02:40 +0530

    animation and moos checkin

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import DashboardPage from './pages/DashboardPage';
+import MoodPage from './pages/MoodPage';
+import MoodCheckinPage from './pages/MoodCheckinPage';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/mood-quick" element={<MoodPage />} />
+        <Route path="/mood" element={<MoodCheckinPage />} />
       </Routes>
     </BrowserRouter>
   );
